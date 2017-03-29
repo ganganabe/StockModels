c555d22256fc2c5dae2e0a1faf8aee8c2687c806
ganganabe <ctdonglin@live.com>
2017-03-29 17:56:53 +0800

    first modify 0308

--- DataToSQL.py
+++ DataToSQL.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
-
+import datetime
 
 class GetDataAndClean(object):
     def __init__(self, date_interval_forward, date_interval_backward):
@@ -14,6 +14,8 @@ class GetDataAndClean(object):
     def GetCodeList(self):
         engine = self.engine
         code_list = pd.read_sql_query("SELECT DISTINCT code FROM t_md_hs_code_k WHERE date >= '2012-01-01'", engine)
+        code_list = code_list.ix[:, 'code']
+        print("Get the code_list")
         return code_list
 
     def GetDataMatrix(self, code):
@@ -25,7 +27,7 @@ class GetDataAndClean(object):
         raw_data.sort_values(by="date")
         date_list = raw_data.ix[:, 'date']
         X_close = ["close_day" + str(i) for i in range(1, date_interval_forward+1)]
-        X_volume = ["close_day" + str(i) for i in range(1, date_interval_forward+1)]
+        X_volume = ["volume_day" + str(i) for i in range(1, date_interval_forward+1)]
         date_code = [str(d) + "_" + str(code) for d in date_list]
         data_close = pd.DataFrame([range(date_interval_forward)], columns=X_close).drop(0, axis=0)
         data_volume = pd.DataFrame([range(date_interval_forward)], columns=X_volume).drop(0, axis=0)
@@ -38,6 +40,7 @@ class GetDataAndClean(object):
             y_judge = (raw_data.ix[x+date_interval_backward, 'close']/raw_data.ix[x, 'close'])/raw_data.ix[x, 'close']
             data_y = data_y.append(pd.Series(y_judge, index=['y'], name=date_code[x]))
         data_matrix = pd.concat([data_close, data_volume, data_y], axis=1)
+#        print("Get the data_matrix")
         return data_matrix
 
     def WtoMysql(self):
@@ -45,8 +48,9 @@ class GetDataAndClean(object):
         engine = self.engine
         for c in code_list:
             data_matrix = self.GetDataMatrix(c)
-            data_matrix.to_sql('data20160101_3030', engine, index_label=['datecode'], index=True, if_exists='append')
+            data_matrix.to_sql('data20160101_3030', engine, index_label=['datecode'], chunksize=1000, index=True, if_exists='append')
             print("stock: %s has been writen to Mysql" % c)
+
 
 if __name__ == '__main__':
     date_interval_forward = 30
